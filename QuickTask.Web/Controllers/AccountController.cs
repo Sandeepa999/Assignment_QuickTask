b74036902ffae8e4bde381d1ac606bbad3c3421f
Sandeepa999 <ashmasandeepa1@gmail.com>
2024-10-24 19:21:52 +0530

    third commit

--- QuickTask.Web/Controllers/AccountController.cs
+++ QuickTask.Web/Controllers/AccountController.cs
@@ -7,10 +7,12 @@ namespace QuickTask.Web.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
 
-        public AccountController(UserManager<IdentityUser> userManager)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
         [HttpGet]
         public IActionResult Register()
@@ -35,6 +37,31 @@ namespace QuickTask.Web.Controllers
 
             }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel) 
+        {
+            var signInResult = await signInManager.PasswordSignInAsync(loginViewModel.Username, loginViewModel.Password, false, false);
+
+            if (signInResult != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
