f4ffdf00b2782d16d6c698439e3e1fcb5918f21d
Sandeepa999 <ashmasandeepa1@gmail.com>
2024-10-24 09:42:19 +0530

    first commit

--- QuickTask.Web/Controllers/JobController.cs
+++ QuickTask.Web/Controllers/JobController.cs
@@ -31,16 +31,65 @@ namespace QuickTask.Web.Controllers
             quickTaskDbContext.Jobs.Add(job);
             quickTaskDbContext.SaveChanges();
 
-            return View();
+            return RedirectToAction("TaskList");
         }
 
         [HttpGet]
-        public IActionResult TaskList() 
+        public IActionResult TaskList()
         {
             var jobs = quickTaskDbContext.Jobs.ToList();
             return View(jobs);
         }
 
+        [HttpGet]
+        public IActionResult TaskEdit(Guid id)
+        {
+            var job = quickTaskDbContext.Jobs.FirstOrDefault(x => x.TaskId == id);
+            if (job != null)
+            {
+                var editTaskRequest = new EditTaskRequest
+                {
+                    TaskId = job.TaskId,
+                    Title = job.Title,
+                    Description = job.Description
+                };
+                return View(editTaskRequest);
+            }
+            return View(null);
+        }
 
+        [HttpPost]
+        public IActionResult TaskEdit(EditTaskRequest editTaskRequest)
+        {
+            var job = new Job
+            {
+                TaskId = editTaskRequest.TaskId,
+                Title = editTaskRequest.Title,
+                Description = editTaskRequest.Description
+            };
+            var existingTask = quickTaskDbContext.Jobs.Find(job.TaskId);
+
+            if (existingTask != null)
+            {
+                existingTask.Title = job.Title;
+                existingTask.Description = job.Description;
+
+                quickTaskDbContext.SaveChanges();
+                return RedirectToAction("TaskList");
+            }
+
+            return View("Edit");
+        }
+
+        [HttpPost]
+        public IActionResult TaskDelete(Guid id)
+        {
+            var job = quickTaskDbContext.Jobs.FirstOrDefault(x => x.TaskId == id);
+
+            quickTaskDbContext.Jobs.Remove(job);
+            quickTaskDbContext.SaveChanges();
+            return RedirectToAction("TaskList");
+
+        }
     }
 }
---
+++ QuickTask.Web/Models/ViewModels/EditTaskRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickTask.Web.Models.ViewModels
+{
+    public class EditTaskRequest
+    {
+        public Guid TaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
